1d8f9f52ec1d53830a4996779f934b9f78e61e77
Heriberto Lopez Jr <86804707+hlj95@users.noreply.github.com>
2021-07-27 17:12:23 -0500

    Update app.js

--- ToDo/scripts/app.js
+++ ToDo/scripts/app.js
@@ -11,11 +11,15 @@ addButton.addEventListener("click", function() {
     const task = taskInput.value;
 
 
-    const taskLI = document.createElement("li");
+    const taskLI = document.createElement("li")
     taskLI.innerHTML = task
-
+    let checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox")
+    checkbox.setAttribute("class", "checkbox")
+    taskLI.appendChild(checkbox)
+    
     const removeButton = document.createElement("button")
-    const completeButton = document.createElement("button")
+    
 
     //REMOVE TASK
     removeButton.addEventListener("click", function() {
@@ -27,27 +31,25 @@ addButton.addEventListener("click", function() {
     tasksUl.appendChild(taskLI)
 
     //Complete Task
-    completeButton.addEventListener("click", function() {
+    checkbox.addEventListener("click", function() {
         tasksUl.removeChild(this.parentElement)
         const completeLI = document.createElement("li")
+        completeLI.innerHTML = task
+        let checkbox = document.createElement("input")
+        checkbox.setAttribute("type", "checkbox")
+        checkbox.setAttribute("class", "checkbox")
+        completeLI.appendChild(checkbox)
         
-        doneTasks.innerHTML = task
-
+        
             removeButton.addEventListener("click", function() {
                 doneTasks.removeChild(this.parentElement)
             })
-        
+
             removeButton.innerHTML = "Remove"
             completeLI.appendChild(removeButton)
             doneTasks.appendChild(completeLI)
+
     })
-
-    completeButton.innerHTML = "Complete"
-    taskLI.appendChild(completeButton)
-    tasksUl.appendChild(taskLI)
-
-
 })
-
 
 
