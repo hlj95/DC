72733642827a940b8f5af4129b6b108a5117af27
Heriberto Lopez Jr <86804707+hlj95@users.noreply.github.com>
2021-07-29 15:57:15 -0500

    Add files via upload

---
+++ News/Scripts/script.js
@@ -0,0 +1,20 @@
+const newsUL = document.getElementById('newsUL')
+
+const newsItem = news.articles.map(function(article) {
+    const allNews = `
+    <div class="news">
+    <li class="newslist">
+        <div class="article">
+            <img src="${article.urlToImage} class = "newsImage" />
+            <b>${article.title}</b>
+            <b>Written By : ${article.author}</b>
+            <p>${article.description}</p>
+            <b><a href = "${article.url}">NewsLink</a></b>
+            <b>Published: ${article.publishedAt}</b>
+            <p>${article.content}</p>
+        </div>
+    </li></div>`
+    return allNews
+})
+
+newsUL.innerHTML = newsItem.join("")
