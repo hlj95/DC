c107cacf442cd0175c76083b9730d4db44e05c3c
Heriberto Lopez Jr <86804707+hlj95@users.noreply.github.com>
2021-07-29 21:10:35 -0500

    Update script.js

--- News/Scripts/script.js
+++ News/Scripts/script.js
@@ -3,17 +3,18 @@ const newsUL = document.getElementById('newsUL')
 const newsItem = news.articles.map(function(article) {
     const allNews = `
     <div class="news">
-    <li class="newslist">
-        <div class="article">
-            <img src="${article.urlToImage} class = "newsImage" />
-            <b>${article.title}</b>
-            <b>Written By : ${article.author}</b>
-            <p>${article.description}</p>
-            <b><a href = "${article.url}">NewsLink</a></b>
-            <b>Published: ${article.publishedAt}</b>
-            <p>${article.content}</p>
-        </div>
-    </li></div>`
+        <li class="newslist">
+            <div class="article">
+                <img src="${article.urlToImage} class = "newsImage" />
+                <b>${article.title}</b>
+                <b>Written By : ${article.author}</b>
+                <p>${article.description}</p>
+                <b><a href = "${article.url}">NewsLink</a></b>
+                <b>Published: ${article.publishedAt}</b>
+                <p>${article.content}</p>
+            </div>
+        </li>
+    </div>`
     return allNews
 })
 
